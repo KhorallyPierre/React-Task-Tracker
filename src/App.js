8cff729a6f84adacae9f562a225d7b4416ede3f0
Khorally Pierre <kvpierre@gmail.com>
2021-05-12 11:47:15 -0400

    task tracker rep

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+function App() {
+  const name = "Corey"
+  const x =true
+  return (
+
+    <div className="container">
+      <h1> Hello from react, khorally </h1>
+
+      <h2> you so fiiiinnneee { x ? 'yes': 'no'} </h2>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = ({title }) => {
+  return (
+    <header>
+    <h1> Task Tracker </h1>
+
+  
+  )
+}
